30bc234ab59f536923414e2a17c0356d8722d75b
root <root@localhost.localdomain>
2018-08-01 00:03:41 +0200

    c'est quand meme pas evident

--- fonction/ft_str_is_alpha.c
+++ fonction/ft_str_is_alpha.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 
 int		ft_str_is_alpha(char *str)
 {
@@ -14,12 +12,4 @@ int		ft_str_is_alpha(char *str)
 			return (0);
 	}
 	return (1);
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc > 2)
-		return (1);
-	printf("%d\n", ft_str_is_alpha(argv[1]));
-	return (0);
 }
