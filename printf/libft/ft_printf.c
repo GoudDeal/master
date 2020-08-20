345e79082e8d770c4073b0d7c3ef062258419f3b
Cyprien DELMAS <dcyprien@e2r1p11.42.fr>
2020-08-20 15:58:38 +0200

    oui

--- printf/libft/ft_printf.c
+++ printf/libft/ft_printf.c
@@ -6,26 +6,11 @@
 /*   By: dcyprien <dcyprien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/28 15:22:05 by dcyprien          #+#    #+#             */
-/*   Updated: 2020/08/18 23:38:45 by dcyprien         ###   ########.fr       */
+/*   Updated: 2020/08/20 15:58:28 by dcyprien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	printest(t_format format)
-{
-	printf("Type = %c\n", format.type);
-	printf("Padding = %d\n", format.padding);
-	printf("Inversed? = %d\n", format.inversed);
-	printf("width = %d\n", format.width);
-	printf("precision = %d\n", format.precision);
-	printf("precision.min = %d\n", format.min);
-	printf("precision_star = %d\n", format.precision_star);
-	printf("precision-size = %d\n", format.prec_size);
-	printf("precision-zero = %d\n", format.precision_default);
-	printf("fill = %c\n", format.fill);
-	printf("Width Star = %d\n\n", format.width_star);
-}
 
 int		ft_printf(const char *str, ...)
 {
